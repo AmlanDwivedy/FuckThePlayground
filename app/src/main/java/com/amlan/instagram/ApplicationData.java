220b44309c1e49355a20facb08638c586a6bae05
Amlan <amlana.dwivedy@ymedialabs.com>
2016-08-08 19:42:15 +0530

    Initial Commit

---
+++ app/src/main/java/com/amlan/instagram/ApplicationData.java
@@ -0,0 +1,10 @@
+package com.amlan.instagram;
+
+/**
+ * Created by amlan on 8/8/16.
+ */
+public class ApplicationData {
+    public static final String CLIENT_ID = "2db49b4ecb9b49c4862c4c8eeb66bc04";
+    public static final String CLIENT_SECRET = "03b88b02aa4f43b7a6cf2b66f3a9897b";
+    public static final String CALLBACK_URL = "http://www.saloncentric.com";
+}
---
+++ app/src/main/java/com/yml/testapp/MainActivity.java
@@ -0,0 +1,216 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.yml.testapp;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String CLOUD_VISION_API_KEY = "AIzaSyCf4nJM2qPtZwAQxpPlHbVpWcIYvv2fc_s";
+    public static final String FILE_NAME = "temp.jpg";
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int GALLERY_IMAGE_REQUEST = 1;
+    public static final int CAMERA_PERMISSIONS_REQUEST = 2;
+    public static final int CAMERA_IMAGE_REQUEST = 3;
+
+    private TextView mImageDetails;
+    private ImageView mMainImage;
+    private TessBaseAPI baseAPI;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        baseAPI = new TessBaseAPI();
+        String sdCard = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+        baseAPI.init(sdCard, "eng");
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder
+                        .setMessage(R.string.dialog_select_prompt)
+                        .setPositiveButton(R.string.dialog_select_gallery, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                startGalleryChooser();
+                            }
+                        })
+                        .setNegativeButton(R.string.dialog_select_camera, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                startCamera();
+                            }
+                        });
+                builder.create().show();
+            }
+        });
+
+        mImageDetails = (TextView) findViewById(R.id.image_details);
+        mMainImage = (ImageView) findViewById(R.id.main_image);
+    }
+
+    public void startGalleryChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select a photo"),
+                GALLERY_IMAGE_REQUEST);
+    }
+
+    public void startCamera() {
+        if (PermissionUtils.requestPermission(
+                this,
+                CAMERA_PERMISSIONS_REQUEST,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA)) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(getCameraFile()));
+            startActivityForResult(intent, CAMERA_IMAGE_REQUEST);
+        }
+    }
+
+    public File getCameraFile() {
+        File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        return new File(dir, FILE_NAME);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            uploadImage(data.getData());
+        } else if (requestCode == CAMERA_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            uploadImage(Uri.fromFile(getCameraFile()));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (PermissionUtils.permissionGranted(
+                requestCode,
+                CAMERA_PERMISSIONS_REQUEST,
+                grantResults)) {
+            startCamera();
+        }
+    }
+
+    public void uploadImage(final Uri uri) {
+        if (uri != null) {
+
+            // scale the image to save on bandwidth
+
+            new AsyncTask<Void, Void, Void>() {
+                Bitmap bitmap;
+                String text;
+
+                @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+                    try {
+                        bitmap =
+                                scaleBitmapDown(
+                                        MediaStore.Images.Media.getBitmap(getContentResolver(), uri),
+                                        1200);
+                        if (bitmap != null)
+                            mMainImage.setImageBitmap(bitmap);
+                        else
+                            Toast.makeText(MainActivity.this, R.string.image_picker_error, Toast.LENGTH_LONG).show();
+                    } catch (IOException e) {
+                        Log.d(TAG, "Image picking failed because " + e.getMessage());
+
+                    }
+                }
+
+                @Override
+                protected Void doInBackground(Void[] params) {
+
+
+                    baseAPI.setImage(bitmap);
+                    text = baseAPI.getUTF8Text();
+                    Log.e("amlan", "Text ----> " + text);
+
+                    return null;
+                }
+
+                @Override
+                protected void onPostExecute(Void o) {
+                    super.onPostExecute(o);
+
+                    if (text != null)
+                        mImageDetails.setText(text);
+                }
+            }.execute();
+
+
+        } else {
+            Log.d(TAG, "Image picker gave us a null image.");
+            Toast.makeText(this, R.string.image_picker_error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public Bitmap scaleBitmapDown(Bitmap bitmap, int maxDimension) {
+
+        int originalWidth = bitmap.getWidth();
+        int originalHeight = bitmap.getHeight();
+        int resizedWidth = maxDimension;
+        int resizedHeight = maxDimension;
+
+        if (originalHeight > originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = (int) (resizedHeight * (float) originalWidth / (float) originalHeight);
+        } else if (originalWidth > originalHeight) {
+            resizedWidth = maxDimension;
+            resizedHeight = (int) (resizedWidth * (float) originalHeight / (float) originalWidth);
+        } else if (originalHeight == originalWidth) {
+            resizedHeight = maxDimension;
+            resizedWidth = maxDimension;
+        }
+        return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false);
+    }
+}
